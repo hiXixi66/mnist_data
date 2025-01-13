42cbb9bf49245244a19f2b66f04aeac00f02a5eb
xixiwang <hixixi66@gmail.com>
2025-01-13 17:43:45 +0100

    add test file

---
+++ tests/test_model.py
@@ -0,0 +1,26 @@
+from src.exercise2_template.model import MyAwesomeModel
+import torch
+
+# def test_model():
+#     model = MyAwesomeModel()
+#     x = torch.randn(1, 1, 28, 28)
+#     y = model(x)
+#     assert y.shape == (1, 10)
+    
+    # tests/test_model.py
+import pytest
+
+# def test_error_on_wrong_shape():
+#     model = MyAwesomeModel()
+#     with pytest.raises(ValueError, match='Expected input to a 4D tensor'):
+#         model(torch.randn(1,2,3))
+#     with pytest.raises(ValueError, match='Expected each sample to have shape [1, 28, 28]'):
+#         model(torch.randn(1,1,28,29))
+@pytest.mark.parametrize("batch_size", [32, 64])
+def test_model(batch_size: int) -> None:
+    model = MyAwesomeModel()
+    x = torch.randn(batch_size, 1, 28, 28)
+    y = model(x)
+    assert y.shape == (batch_size, 10)
+
+# test_model(32)
